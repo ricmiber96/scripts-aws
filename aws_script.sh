8689055a1aa3dda72e1a1cd251032bb141f6f069
ricmiber96 <ricmiber@gmail.com>
2025-10-29 19:19:33 +0100

    Update: Actualizado script habilitando dns en la vpc

--- aws_script.sh
+++ aws_script.sh
@@ -1,3 +1,13 @@
-aws ec2 create-vpc --cidr-block 192.168.0.0/24 \
-    --tag-specifications 'ResourceType=vpc,Tags=[{Key=Name,Value=MyVpc}]' \
-    --query Vpc.VpcId --output text
+#Creo la VPC y devuelvo su ID 
+VPC_ID=$(aws ec2 create-vpc --cidr-block 192.168.0.0/24 \
+    --tag-specifications 'ResourceType=vpc,Tags=[{Key=Name,Value=VPCRicardo}]' \
+    --query Vpc.VpcId --output text)
+
+#Muestro la ID de la VPC
+echo $VPC_ID
+
+#habilitar dns en la vpc
+aws ec2 modify-vpc-attribute \
+    --vpc-id $VPC_ID \
+    --enable-dns-hostnames "{\"Value\":true}"
+
